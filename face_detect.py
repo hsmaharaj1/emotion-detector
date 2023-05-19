1ed906a455ce866d78a7dd1f580c7fa94f59a90e
Himanshu Sekhar <54274314+hsmaharaj1@users.noreply.github.com>
2023-05-20 02:11:35 +0530

    Add files via upload

--- face_detect.py
+++ face_detect.py
@@ -1,6 +1,10 @@
 import cv2
+import tensorflow as tf
+import numpy as np
 
+emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 face_detection_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+emotion_model = tf.keras.models.load_model("fer2013_mini_XCEPTION.102-0.66.hdf5")
 
 video = cv2.VideoCapture(0)
 
@@ -8,15 +12,34 @@ while True:
     chk, frame = video.read()
     grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    faces = face_detection_model.detectMultiScale(grey_frame, scaleFactor=1.1, minNeighbors=6)
+    faces = face_detection_model.detectMultiScale(grey_frame, 1.2, 6)
 
     for (x,y,width,height) in faces:
-        cv2.rectangle(frame, (x,y), (x+width, y+height), (255, 255, 255), 3)
+        face_image = grey_frame[y:y+height, x:x+width]
+        # cv2.rectangle(frame, (x,y), (x+width, y+height), (255, 255, 255), 3)
 
-    cv2.imshow('video', frame)
+        #Preprocessing
+        face_image = cv2.resize(face_image, (64,64))
+        face_image = np.expand_dims(face_image, axis=0)
+        face_image = np.expand_dims(face_image, axis=-1)
+        face_image = face_image/255
 
-    key = cv2.waitKey(30) & 0xff
-    if key == 27:
+        emotion_predict = emotion_model.predict(face_image)[0]
+        max_index = np.argmax(emotion_predict)
+        emotion_label = np.argmax(emotion_predict)
+        preicted_data=emotions[emotion_label]
+        emotion_probab = emotion_predict[max_index]
+        probability_percentages = f"{emotion_probab*100:.2f}%"
+
+        cv2.rectangle(frame, (x,y), (x+width, y+height), (1,255,23), 3)
+        cv2.putText(frame, preicted_data, (x,y-10), cv2.FONT_HERSHEY_DUPLEX, 0.9, (36,255,1), 2)
+        cv2.putText(frame, probability_percentages, (x, y+height+25), cv2.FONT_ITALIC, 0.6, (255, 255, 255), 2)
+
+
+    cv2.imshow('CAM', frame)
+
+    key = cv2.waitKey(1)
+    if key == 27 or cv2.getWindowProperty('CAM', cv2.WND_PROP_VISIBLE)<1:
         break
 
 video.release()
